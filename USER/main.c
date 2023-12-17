d6f47329ff2f610cd2ee653932b5bb8a04fecda6
user_zlb <744833420@qq.com>
2023-12-17 18:23:02 +0800

    FreeRTOS clion 标准库 初始化正版

--- USER/main.c
+++ USER/main.c
@@ -1,32 +1,42 @@
-#include"sys.h"
+#include "sys.h"
+
+
+static TaskHandle_t app_task1_handle = NULL;
+
+/* 任务1 */
+static void app_task1(void* pvParameters);
+
+
 int main(void)
 {
-	u32 t=0;
-    u8 len;
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
-	uart_init(115200);
-	delay_init(84);
-	LED_Init();
-  while(1){
-      if(USART_RX_STA&0x8000)
-      {
-          len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
-          printf("\r\n您发送的消息为:\r\n");
-          for(t=0;t<len;t++)
-          {
-              USART_SendData(USART1, USART_RX_BUF[t]);         //向串口1发送数据
-              while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
-          }
-          printf("\r\n\r\n");//插入换行
-          USART_RX_STA=0;
-      }
-        printf("%s\r\n",USART_RX_BUF);
-        PFout(9)=!PFout(9);
-        PFout(10)=!PFout(10);
-		delay_ms(5000);
-		t++;
-        printf("请输入你的数字\r\n");
-        //scanf("%d",&t);
-	}
+
+
+    LED_Init();
+
+    /* 创建app_task1任务 */
+    xTaskCreate((TaskFunction_t )app_task1,  		/* 任务入口函数 */
+                (const char*    )"app_task1",			/* 任务名字 */
+                (uint16_t       )512,  				/* 任务栈大小 */
+                (void*          )NULL,				/* 任务入口函数参数 */
+                (UBaseType_t    )4, 					/* 任务的优先级 */
+                (TaskHandle_t*  )&app_task1_handle);	/* 任务控制块指针 */
+
+
+
+    /* 开启任务调度 */
+    vTaskStartScheduler();
+
+}
+
+static void app_task1(void* pvParameters)
+{
+    for(;;)
+    {
+
+        GPIO_ToggleBits(GPIOF, GPIO_Pin_9);
+        GPIO_ToggleBits(GPIOF, GPIO_Pin_10);
+        vTaskDelay(1000);
+
+    }
 }
 
